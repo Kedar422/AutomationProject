adb7a0fd8b216c9bef89ab94ccb3ca80a98be3e3
kedar422 <kedarnathreddy07@gmail.com>
2021-09-15 09:30:34 +0530

    first commit

---
+++ automation/src/main/java/Resource/Base.java
@@ -0,0 +1,25 @@
+package Resource;
+
+
+	import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.chrome.ChromeDriver;
+
+	public class Base {
+		
+		
+		public WebDriver driver=null;
+		
+		public WebDriver initializeDriver()  {
+			
+			System.setProperty("webdriver.chrome.driver","C://chromedriver.exe" );
+			driver =new ChromeDriver();
+			return driver;
+		}
+	}
+
+
+		
+
+
+
+
